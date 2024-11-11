4ad5dbeaa7ba20dfb6d1983dad5dd57898325290
MichaSchrader <62886682+MichaSchrader@users.noreply.github.com>
2024-11-11 16:10:24 +0100

    initialized the project with spring initializr

---
+++ src/main/java/de/michaschrader/planetdistance/PlanetdistanceApplication.java
@@ -0,0 +1,13 @@
+package de.michaschrader.planetdistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetdistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetdistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/michaschrader/planetdistance/PlanetdistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.michaschrader.planetdistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetdistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
