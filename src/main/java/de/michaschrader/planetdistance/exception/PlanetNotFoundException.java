3aee0ff8d9445a2ee93e8c12f346c52be155cdee
MichaSchrader <62886682+MichaSchrader@users.noreply.github.com>
2024-11-24 16:37:16 +0100

    moved backend to it's own repository at https://github.com/MichaSchrader/planetdistance-api

--- src/main/java/de/michaschrader/planetdistance/exception/PlanetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.michaschrader.planetdistance.exception;
-
-public class PlanetNotFoundException extends RuntimeException{
-    public PlanetNotFoundException(String message) {
-        super(message);
-    }
-}
